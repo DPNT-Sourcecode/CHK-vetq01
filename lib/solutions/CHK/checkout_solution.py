a1f78df0d15ad41df70eb79c904a01757059f5a3
sbx_user1051 <sbx_user1051@169.254.2.21>
2024-04-29 17:51:46 +0000

    Mon Apr 29 17:51:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,39 @@
+from collections import defaultdict
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # Create a dictionary that has keys as the product names and the values as their prices
+    product_costs = {"A": 50, "B": 30, "C": 20, "D": 15}
+    # Create a dictionary that has product names as keys and  multi-price offers as the values
+    product_multi_value_costs = defaultdict(list)
+    product_multi_value_costs["A"] = [3, 130]
+    product_multi_value_costs["B"] = [2, 45]
+    # Create a frequency dictionary of skus given in parameter
+    sku_frequency = create_frequency_dictionary(skus)
+    total_cost = 0
+    for sku in sku_frequency:
+        quantity = sku_frequency[sku]
+        # If product on offer, check quantity and apply cost accordingly
+        if sku in product_multi_value_costs:
+            if quantity == product_multi_value_costs[sku][0]:
+                total_cost += product_multi_value_costs[sku][1]
+            else:
+                total_cost += product_costs[sku]
+        elif sku in product_costs:
+            total_cost += product_costs[sku]
+        # Return -1 in case the product does not exist
+        else:
+            return -1
+    return total_cost
+
+def create_frequency_dictionary(skus):
+    frequency_dictionary = {}
+    for sku in skus:
+        if sku in frequency_dictionary:
+            frequency_dictionary[sku] += 1
+        else:
+            frequency_dictionary[sku] = 1
+    return frequency_dictionary
+
--- test/solution_tests/CHK/test_chck_r1.py
+++ test/solution_tests/CHK/test_chck_r1.py
@@ -1,8 +1,19 @@
 import unittest
+from lib.solutions.CHK.checkout_solution import checkout
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_without_offer(self):
+        skus = "ABC"
+        self.assertEqual(checkout(skus), 100)
+
+    def test_with_offer(self):
+        skus = "AAABCD"
+        self.assertEqual(checkout(skus), 195)
+
+    def test_with_invalid_product(self):
+        skus = "AAAABCZ"
+        self.assertEqual(checkout(skus), -1)
 
 if __name__ == '__main__':
     unittest.main()
