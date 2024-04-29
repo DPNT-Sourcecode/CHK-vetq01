1d50a074d0054878341ba69b13094625e6dc22cd
sbx_user1051 <sbx_user1051@169.254.2.21>
2024-04-29 20:08:00 +0000

    Mon Apr 29 20:08:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -62,5 +62,3 @@ def create_frequency_dictionary(skus):
             frequency_dictionary[sku] = 1
     return frequency_dictionary
 
-
-
--- test/solution_tests/CHK/test_chck_r2.py
+++ test/solution_tests/CHK/test_chck_r2.py
@@ -46,5 +46,3 @@ class MyTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
