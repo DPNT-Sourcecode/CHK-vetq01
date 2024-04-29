3797e991da293dd99be6e8e1bb7bee6f0b73db8e
sbx_user1051 <sbx_user1051@169.254.2.21>
2024-04-29 18:16:16 +0000

    Mon Apr 29 18:16:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,33 +4,37 @@ from collections import defaultdict
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+# dictionary that has keys as the product names and the values as their prices
+PRODUCT_COSTS = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+# dictionary that has product names as keys and  multi-price offers as the values, ordered by quantity
+PRODUCT_MULTI_VALUE_COSTS = defaultdict(list)
+PRODUCT_MULTI_VALUE_COSTS["A"] = [[5, 200], [3, 130]]
+PRODUCT_MULTI_VALUE_COSTS["B"] = [[2, 45]]
+PRODUCT_MULTI_VALUE_COSTS["E"] = [[2, -30]]
 def checkout(skus):
-    # Create a dictionary that has keys as the product names and the values as their prices
-    product_costs = {"A": 50, "B": 30, "C": 20, "D": 15}
-    # Create a dictionary that has product names as keys and  multi-price offers as the values
-    product_multi_value_costs = defaultdict(list)
-    product_multi_value_costs["A"] = [3, 130]
-    product_multi_value_costs["B"] = [2, 45]
     # Create a frequency dictionary of skus given in parameter
     sku_frequency = create_frequency_dictionary(skus)
     total_cost = 0
     for sku in sku_frequency:
         quantity = sku_frequency[sku]
         # If product on offer, check quantity and apply cost accordingly
-        if sku in product_multi_value_costs:
-            multi_offer_threshold = product_multi_value_costs[sku][0]
-            multi_offer_price = product_multi_value_costs[sku][1]
-            # Check how many multi-buy offers we have
-            multi_buy = math.floor(quantity / multi_offer_threshold)
-            # Check product outwith the offer
-            single_buy = quantity % multi_offer_threshold
-            total_cost = total_cost + (multi_offer_price * multi_buy) + (single_buy * product_costs[sku])
-        elif sku in product_costs:
-            total_cost += product_costs[sku] * quantity
+        if sku in PRODUCT_MULTI_VALUE_COSTS:
+            # iterate through all offers for sku
+            for quantity_threshold, offer in PRODUCT_MULTI_VALUE_COSTS[sku]:
+                multi_buy = math.floor(quantity / quantity_threshold)
+                total_cost += multi_buy * offer
+                quantity -= multi_buy * quantity_threshold
+            # If product remaining, using single buy costs
+            if quantity > 0:
+                total_cost += quantity * PRODUCT_COSTS[sku]
+
+        elif sku in PRODUCT_COSTS:
+            total_cost += PRODUCT_COSTS[sku] * quantity
         # Return -1 in case the product does not exist
         else:
             return -1
     return total_cost
+
 
 def create_frequency_dictionary(skus):
     frequency_dictionary = {}
@@ -40,4 +44,5 @@ def create_frequency_dictionary(skus):
         else:
             frequency_dictionary[sku] = 1
     return frequency_dictionary
+
 
