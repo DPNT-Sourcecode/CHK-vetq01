286b89b1d216f8deaf723f7f126e12a76aff0c61
sbx_user1051 <sbx_user1051@169.254.2.21>
2024-04-29 23:12:53 +0000

    Mon Apr 29 23:12:53 UTC 2024

--- test/solution_tests/CHK/test_chck_r5#.py
+++ test/solution_tests/CHK/test_chck_r5#.py
@@ -1,8 +1,19 @@
 import unittest
+from lib.solutions.CHK.checkout_solution import checkout
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def test_invalid_deal(self):
+        skus = "SSSTTT"
+        self.assertEqual(checkout(skus), 120)
+
+    def test_valid_deal(self):
+        skus = "SSSTTTXXX"
+        self.assertEqual(checkout(skus), 135)
+
+
 
 if __name__ == '__main__':
     unittest.main()
+
