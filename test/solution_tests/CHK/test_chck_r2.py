d7c3473ed229fa0f24e2ca5411c852728a24e5bd
sbx_user1051 <sbx_user1051@169.254.2.21>
2024-04-29 18:37:16 +0000

    Mon Apr 29 18:37:16 UTC 2024

--- test/solution_tests/CHK/test_chck_r2.py
+++ test/solution_tests/CHK/test_chck_r2.py
@@ -1,11 +1,21 @@
 import unittest
+from lib.solutions.CHK.checkout_solution import checkout
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_product_with_mutiple_offers(self):
+        skus = "AAAAAAAA"
+        self.assertEqual(checkout(skus), 330)
 
+    def test_product_with_product_offer(self):
+        skus = "AAAEE"
+        self.assertEqual(checkout(skus), 210)
+
+    def test_product_with_multiple_instances(self):
+        skus = "AAAAAAAAAA"
+        self.assertEqual(checkout(skus), 400)
 
 if __name__ == '__main__':
     unittest.main()
+
 
